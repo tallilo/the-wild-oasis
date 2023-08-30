c24cbaa265454c9f7f0021188e13349472db24b2
tallilo <tallilo5011323@gmail.com>
2023-08-30 10:58:44 +0300

    bug fix-1

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -3,7 +3,7 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 import AddCabin from "../features/cabins/AddCabin";
-import CabinTableOperations from "../features/cabins/cabinTableOperations";
+import CabinTableOperations from "./../features/cabins/CabinTableOperations";
 
 function Cabins() {
   return (
